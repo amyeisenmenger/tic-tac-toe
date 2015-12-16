01c6a8bb0153299962c712a45675e007a39a72e7
Amy Eisenmenger <amy.eisenmenger@gmail.com>
2015-12-15 17:58:40 -0700

    clear button

--- main.js
+++ main.js
@@ -23,8 +23,8 @@ for(var i = 0; i < boxes.length; i++) {
           this.classList.add('filled-box');
           this.innerHTML = mouse ;
           changeMouse();
-          endOfGame();
       }
+      endOfGame();
     });
   };
 
@@ -34,41 +34,30 @@ function checkWin(){
   if (boxes[0].id in filledBoxes) {
     if (boxes[0].innerHTML == boxes[1].innerHTML && boxes[0].innerHTML == boxes[2].innerHTML) {
       return true;
-      // alert('Game Over: ' + boxes[0].innerHTML + "s WIN");
     } else if (boxes[0].innerHTML == boxes[4].innerHTML && boxes[0].innerHTML == boxes[8].innerHTML){ 
-      // alert('Game Over c ');
       return true;
     } else if (boxes[0].innerHTML == boxes[3].innerHTML && boxes[0].innerHTML == boxes[6].innerHTML) {
-      // alert('game over d');
       return true;
     } else {
       return false;
     };
-// 1, 4, 7
   } else if (boxes[1].id in filledBoxes && boxes[1].innerHTML == boxes[4].innerHTML && boxes[1].innerHTML == boxes[7].innerHTML) {
-    // alert('game over e');
     return true;
-// 2,5,8
   } else if (boxes[2].id in filledBoxes){
     if (boxes[2].innerHTML == boxes[5].innerHTML && boxes[2].innerHTML == boxes[8].innerHTML) {
-      // alert('game over f');
       return true;
       // 2,4,6
     }else if (boxes[2].innerHTML == boxes[4].innerHTML && boxes[2].innerHTML == boxes[6].innerHTML){
-      // alert('game over g');
       return true;
     } else {
       return false;
     };
-// 3,4,5
   } else if (boxes[3].id in filledBoxes && boxes[3].innerHTML == boxes[4].innerHTML && boxes[3].innerHTML == boxes[5].innerHTML) {
-    // alert('game over h');
     return true;
-    // 6,7,8
   } else if (boxes[6].id in filledBoxes && boxes[6].innerHTML == boxes[7].innerHTML && boxes[6].innerHTML == boxes[8].innerHTML) {
-    // alert('game over i');
     return true;
-
+  } else if (boxes.length == filledBoxes.length) {
+    return true;
   } else {
     return false;
   };
@@ -83,7 +72,21 @@ function endOfGame(){
   };
 };
 
-// endOfGame();
-// clickBox();
-// alert Game Over, who wins
+function resetBoard(){
+  for (var i = 0; i < boxes.length; i++) {
+      boxes[i].innerHTML = null ;
+    };
+}
+
+var resetButton = document.getElementById('reset_button');
+
+resetButton.addEventListener('click', function() {
+  for (var i = 0; i < boxes.length ; i++) {
+    var box = boxes[i];
+    box.innerHTML = null;
+    box.classList.add('active-box');
+    box.classList.remove('filled-box');
+  };
+});
+
 // change all other boxes class to filled-box
